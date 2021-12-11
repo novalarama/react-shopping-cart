45b1a84c0a1e2e7e48d9f6130ca4fd3b7373b201
Noval AKbar Ramadhany <novalakbarramadhany@gmail.com>
2021-12-11 13:33:13 +0700

    cart-component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import formatCurrency from '../util';
+import formatCurrency  from '../util';
 
 export default class Products extends Component {
     render() {
@@ -17,7 +17,7 @@ export default class Products extends Component {
                                 </a>
                                 <div className='product-price'>
                                     <div>{formatCurrency(product.price)}</div>
-                                    <button className="button primary">Add to Cart</button>
+                                    <button onClick={() => this.props.addToCart(product)} className="button primary">Add to Cart</button>
                                 </div>
                             </div>
                         </li>
